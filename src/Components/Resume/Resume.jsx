9295e345e462a34bdfe97a91b83f2c64a53dc79d
Jude <jelijreid27@gmail.com>
2024-05-29 21:54:47 -0500

    Updated resume path

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -8,7 +8,7 @@ const Resume = () => {
         <div className="resume-bar" data-aos='fade' data-aos-easing='ease-in'>
             <p>
                  Take a copy of my resume before you go!</p>
-        <a href="/src/assets/jude-resume24.pdf" download>Download
+        <a href="/jude-resume24.pdf" download>Download
         <img className='downloadImg' src="/download.png" alt="download" /></a>
         </div>
     </div>
